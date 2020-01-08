dc49a0c84f58f5dd52f0ff275f86524a5934606b
Bojan <bojanlalic15@gmail.com>
2020-01-08 23:04:45 +0100

    created frontend for my app

---
+++ todo-frontend/src/views/ListView.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- views folder sluzi da se objedini nekoliko komponenti u celoviti prikaz -->
+  <div>
+    
+    <Navbar />
+    <List />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import List from "@/components/List.vue";
+export default {
+
+  components: {
+    Navbar,
+    List
+  }
+};
+</script>
+
+<style scoped>
+</style>
